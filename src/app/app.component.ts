91e14949b1bdb03b21f58c4408fff3ac15c771cc
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-11-27 02:55:30 -0500

    142. Rutas de nuestra aplicación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,4 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'pipesApp';
 
-  nombre:string = "EndeRson MaRin";
-  valor:number = 5000;
-  obj = {
-    documento: 123456789,
-    telefono: 123456789
-  }
-
-  mostrarPorConsola(){
-    console.log(this.nombre)
-    console.log(this.valor)
-    console.log(this.obj)
-  }
 }
