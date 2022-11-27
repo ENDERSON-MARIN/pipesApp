7e3992112423de091e71ce9f93b5c1b97db7f80a
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-11-27 18:45:41 -0500

    159. Async Pipe

--- src/app/ventas/pages/no-comunes/no-comunes.component.ts
+++ src/app/ventas/pages/no-comunes/no-comunes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-no-comunes',
@@ -6,7 +7,6 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class NoComunesComponent {
-
   // i18nSelect Pipe
   nombre: string = 'Enderson';
   genero: string = 'masculino';
@@ -18,42 +18,51 @@ export class NoComunesComponent {
 
   // i18nPlural Pipe
   clientes: string[] = ['Maria', 'Jose', 'Pedro', 'Miguel', 'Eduardo'];
-  clientesMapa={
-    '=0':'No tenemos ningun cliente esperando',
-    '=1':'tenemos un cliente esperando',
-    '=2':'tenemos dos clientes esperando',
-    'other':'tenemos # clientes esperando'
+  clientesMapa = {
+    '=0': 'No tenemos ningun cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos dos clientes esperando',
+    other: 'tenemos # clientes esperando',
+  };
+
+  cambiarCliente() {
+    this.nombre = 'Susana';
+    this.genero = 'femenino';
   }
 
-  cambiarCliente(){
-    this.nombre = "Susana"
-    this.genero = "femenino"
-  }
-
-  eliminarCliente(){
-    this.clientes.pop()
+  eliminarCliente() {
+    this.clientes.pop();
   }
 
   //Keyvalue Pipe
   persona = {
-    nombre:"Enderson",
-    edad:35,
-    Dirección: "Medellín, Colombia"
-  }
+    nombre: 'Enderson',
+    edad: 35,
+    Dirección: 'Medellín, Colombia',
+  };
 
   //Json Pipe
   heroes = [
-   {
-    'nombre':"Superman",
-    'vuela': true
-   },
-   {
-    'nombre':"Robin",
-    'vuela': false
-   },
-   {
-    'nombre':"Spiderman",
-    'vuela': true
-   }
-  ]
+    {
+      nombre: 'Superman',
+      vuela: true,
+    },
+    {
+      nombre: 'Robin',
+      vuela: false,
+    },
+    {
+      nombre: 'Spiderman',
+      vuela: true,
+    },
+  ];
+
+  //Async Pipe
+  miObservable = interval(1000);
+
+  miPromesa = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data de promesa');
+    }, 3500);
+  });
 }
