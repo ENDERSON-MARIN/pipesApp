95207b3a7200ce859e7d1abb1a7efb093a20109a
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-11-27 20:15:18 -0500

    167. Llenar un PrimeTable con data

--- src/app/ventas/pages/ordenar/ordenar.component.ts
+++ src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
   selector: 'app-ordenar',
@@ -13,6 +14,35 @@ export class OrdenarComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  heroes:Heroe[] = [
+    {
+     nombre:"Superman",
+     vuela:true,
+     color:Color.azul
+    },
+    {
+      nombre:"Batman",
+      vuela:false,
+      color:Color.negro
+    },
+    {
+      nombre:"Robin",
+      vuela:false,
+      color:Color.amarillo
+    },
+    {
+      nombre:"Linterna Verde",
+      vuela:true,
+      color:Color.verde
+    },
+    {
+      nombre:"Spiderman",
+      vuela:true,
+      color:Color.rojo
+    },
+
+  ]
 
   toggle(){
     this.mayuscula = !this.mayuscula
