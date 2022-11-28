ab313a42b0c9905c82dbfaa7e365125e592f2417
Enderson Marín <46828206+ENDERSON-MARIN@users.noreply.github.com>
2022-11-27 20:02:07 -0500

    166. PrimeTable y PrimeToolbar

--- src/app/prime-ng/prime-ng.module.ts
+++ src/app/prime-ng/prime-ng.module.ts
@@ -7,6 +7,8 @@ import {ButtonModule} from 'primeng/button';
 import {CardModule} from 'primeng/card';
 import {FieldsetModule} from 'primeng/fieldset';
 import {MenubarModule} from 'primeng/menubar';
+import {TableModule} from 'primeng/table';
+import {ToolbarModule} from 'primeng/toolbar';
 
 
 @NgModule({
@@ -19,6 +21,8 @@ import {MenubarModule} from 'primeng/menubar';
     CardModule,
     FieldsetModule,
     MenubarModule,
+    TableModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
